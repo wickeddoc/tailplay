43fc7d56cea15f6195a9436c6a8c359c38aa258f
wickeddoc <me@wickeddoc.com>
2022-12-28 17:33:54 +0100

    AsyncJsonWebsocketConsumer automatically handles JSON en-/decoding

--- chats/consumers.py
+++ chats/consumers.py
@@ -1,8 +1,8 @@
 import json
-from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
 
-class ChatConsumer(AsyncWebsocketConsumer):
+class ChatConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
         self.roomGroupName = "group_chat_gfg"
         await self.channel_layer.group_add(
@@ -17,18 +17,17 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.channel_layer
         )
 
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
+    async def receive_json(self, text_data_json):
         message = text_data_json["message"]
         username = text_data_json["username"]
         await self.channel_layer.group_send(
             self.roomGroupName, {
-                "type": "sendMessage",
+                "type": "sendMessageType",
                 "message": message,
                 "username": username,
             })
 
-    async def sendMessage(self, event):
+    async def sendMessageType(self, event):
         message = event["message"]
         username = event["username"]
-        await self.send(text_data=json.dumps({"message": message, "username": username}))
+        await self.send_json({"message": message, "username": username or "Kleeschen"})
